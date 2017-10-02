25eb630c10906cac370101b451cec986fd9a975d
mopuru <mopuru@192.168.0.12>
2017-10-02 16:55:24 +0530

    firstcommit

---
+++ example.rb
@@ -0,0 +1,14 @@
+require 'watir'
+
+browser = Watir::Browser.new :ie
+
+puts "Step 1:  Go to the watir examples homepage"
+browser.goto "http://watir.com/examples/forms_with_input_elements.html"
+
+
+browser.text_field(name: 'new_user_first_name').set 'Harish'
+browser.text_field(name: 'new_user_last_name').set 'Mopuru'
+browser.text_field(name: 'new_user_email').set 'harishmopuri@gmail.com'
+puts "Step 1:  Go to the watir examples homepage"
+
+
